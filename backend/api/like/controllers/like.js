a72675d0675c37659f013aefd8674e148a406dba
Siddhant Singh <ssiddhant9811@gmail.com>
2021-06-18 20:25:24 +0530

    finished backend

--- backend/api/like/controllers/like.js
+++ backend/api/like/controllers/like.js
@@ -8,37 +8,55 @@ const { create } = require('../../post/controllers/post');
  */
 
 module.exports = {
-    async delete(ctx){
-        const {id} = ctx.params;
-        let entity;
-        const [like] = await strapi.services.like.find({
-          id: ctx.params.id,
-          'author.id': ctx.state.user.id,
-        });
-    
-        if(!like){
-          return ctx.unauthorized("You can't delete this entry")
-        }
-    
-        else{
-          const entity = await strapi.services.like.delete({ id });
-        }
-        return sanitizeEntity(entity, { model: strapi.models.like });
-        
-    
-      },
-    async create(ctx){
-        let entity
-        const [like] = await strapi.services.like.find({
-            'author.id': ctx.state.user.id,
-            'post.id': ctx.request.body.post.id
-          });
-        if(!like){
-            entity = await strapi.services.post.create(ctx.request.body)
-        }
+  async delete(ctx){
+    const {id} = ctx.params;
+    let entity;
+    const [like] = await strapi.services.like.find({
+      id: ctx.params.id,
+      'author.id': ctx.state.user.id,
+    });
 
-        else{
-            return ctx.unauthorized("You already liked this")
-        }
+    if(!like){
+      return ctx.unauthorized("You can't delete this entry")
     }
+
+    else{
+      const entity = await strapi.services.like.delete({ id });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.like });
+      
+    
+  },
+  async create(ctx){
+    let entity; 
+    const {id} = ctx.params;
+    const [like] = await strapi.services.like.find({
+      id: ctx.params.id,
+      'author.id': ctx.state.user.id,
+    });
+    if(!like){
+
+      if (ctx.is("multipart")){
+        const {data,files} = parseMultipartData(ctx);
+        data.author = ctx.state.user.id;
+        entity = await strapi.services.like.create(data,{files});
+        
+      }
+      else {
+        ctx.request.body.author = ctx.state.user.id;
+        entity = await strapi.services.like.create(ctx.request.body)
+      }
+    }
+    else{
+      return ctx.unauthorized("You already liked this")
+    }
+    return sanitizeEntity(entity, { model: strapi.models.like });
+  },
+  async count(ctx) {
+    let post = ctx.request.body.post
+    const like = await strapi.services.like.find({
+      'post.id': post,
+    });
+    return like.length
+  },
 };
