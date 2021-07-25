775dd1dd6269084555cb74629dd5ec6fd677327e
Captain Cuckoo <captain_cuckoo@protonmail.com>
2021-07-25 19:47:59 +0530

    text post almost complete

--- frontend/src/components/TextPost.js
+++ frontend/src/components/TextPost.js
@@ -1,8 +1,29 @@
 const TextPost = ({content,auth}) => {
+    const image_url = "http://localhost:1337"+content.author.image.url
+    console.log(content)
     return ( 
         <div className="textpost post">
-            {content.title}
+            <div className="image" style={{background:`url("${image_url}")`, backgroundSize:"cover",backgroundPosition:"center"}}></div>
+            <div className="info">
+                <div className="username">
+                    {content.author.username}
+                    
+                </div>
+                <div className="title">
+                    {content.title}
+                </div>
+                <div className="content">
+                    {content.content}
+                </div>
+            </div>
+            <div className="annoyingshit">
+
+            <div className="bottom">
+                
+            </div>
+            </div>
         </div>
+        
      );
 }
  
