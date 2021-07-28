335d72f5bec4f44ed6a6125818f66f9e0c545a79
Captain Cuckoo <captain_cuckoo@protonmail.com>
2021-07-29 02:06:02 +0530

    restructured textpost

--- frontend/src/components/TextPost.js
+++ frontend/src/components/TextPost.js
@@ -1,27 +1,33 @@
 const TextPost = ({content,auth}) => {
     const image_url = "http://localhost:1337"+content.author.image.url
-    console.log(content)
+
     return ( 
         <div className="textpost post">
-            <div className="image" style={{background:`url("${image_url}")`, backgroundSize:"cover",backgroundPosition:"center"}}></div>
-            <div className="info">
-                <div className="username">
-                    {content.author.username}
-                    
+            <div className="top">
+                <div className="image" style={{background:`url("${image_url}")`, backgroundSize:"cover",backgroundPosition:"center"}}></div>
+                <div className="info">
+                    <div className="username">
+                        {content.author.username}
+                        
+                    </div>
+                    <div className="title">
+                        {content.title}
+                    </div>
+                    <div className="content">
+                        {content.content}
+                    </div>
                 </div>
-                <div className="title">
-                    {content.title}
-                </div>
-                <div className="content">
-                    {content.content}
-                </div>
-            </div>
+            </div> 
             <div className="annoyingshit">
-
             <div className="bottom">
-                
+                Bottom
             </div>
             </div>
+            <div className="comments">
+            Comments
+            </div>
+            
+            
         </div>
         
      );
